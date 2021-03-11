cf681b9937509b13d27013f8df84466360587d4b
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2021-03-11 14:13:34 +0300

    first commit

---
+++ AmazonS3DepolamaApi/Controllers/S3Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonS3DepolamaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class S3Controller:ControllerBase
+    {
+        private readonly IS3Service _service;
+        public S3Controller(IS3Service service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("{kovaadi}")]
+        public async Task<IActionResult> YeniKova([FromRoute] string kovaadi)
+        {
+            var response = await _service.CreateBucketAsync(kovaadi);
+
+            return Ok(response);
+        }
+
+    }
+}
